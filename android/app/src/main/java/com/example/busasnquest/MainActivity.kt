ae4d87b719f3aa823c777b0fce659c89ddb6527c
jeongwy04 <jwy93266372@gmail.com>
2026-06-29 23:18:40 +0900

    지도 연동 성공

--- android/app/src/main/java/com/example/busasnquest/MainActivity.kt
+++ android/app/src/main/java/com/example/busasnquest/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import com.example.busasnquest.ui.navigation.BusanQuestApp
+import com.kakao.vectormap.KakaoMapSdk
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 카카오맵 초기화 (네이티브 앱 키)
+        KakaoMapSdk.init(this, "5f26abd73b4e5c4273ed4ba4ea26aa7e")
 
         setContent {
             MaterialTheme {

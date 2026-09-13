3fc608395c7ed2c29bd0ce5bced9b7ea5f166af5
sihyeonb88@gmail.com < GitHub >
2026-09-13 17:53:28 +0900

    Bump Android app version to 1.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "kr.co.busanquest"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
